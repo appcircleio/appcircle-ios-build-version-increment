f1a0f9d77b8181b17a45ebe637a4503769eb4ad5
Dirk Moors <dirk@Dirks-MacBook-Pro.local>
2023-04-18 09:50:32 +0200

    Making sure "keep" strategy keeps the actual version number

--- main.rb
+++ main.rb
@@ -286,7 +286,10 @@ if version_number_source.nil? and version_strategy == 'keep'
   puts "No version number source specified and strategy is keep. Skipping version number update"
 else
   current_version_number = get_version_number(params, version_number_source)
-  next_version_number = calculate_version_number(current_version_number, version_strategy, omit_zero, version_offset)
+  if version_strategy == 'keep'
+    next_version_number = current_version_number
+  else
+    next_version_number = calculate_version_number(current_version_number, version_strategy, omit_zero, version_offset)
   puts "Next version: #{next_version_number}  Reason -> Source: #{version_number_source} Strategy: #{version_strategy} Omit zero: #{omit_zero} Offset: #{version_offset}"
   increment_key(params, 'CFBundleShortVersionString', next_version_number,'MARKETING_VERSION')
 end
